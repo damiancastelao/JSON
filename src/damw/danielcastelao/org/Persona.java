68827ebad50f9acd712734089108e241d3a68dac
Damian Nogueiras <damian@danielcastelao.org>
2023-05-25 06:57:50 +0200

    Datos con ArrayList

--- src/damw/danielcastelao/org/Persona.java
+++ src/damw/danielcastelao/org/Persona.java
@@ -1,16 +1,34 @@
 package damw.danielcastelao.org;
 
+import java.util.ArrayList;
+
+/**
+ * Clase de datos
+ * Estos son los datos que quiero guardar
+ * en formato json en un fichero
+ * para luego recuperarlos
+ */
 public class Persona {
 
     private String nombre;
     private int edad;
+    private ArrayList<String> hobbies = new ArrayList<>();
 
+    /**
+     * Constructor por defecto con datos
+     */
     public Persona() {
+        this.nombre = "Anonimo";
+        this.edad = 33;
+        this.hobbies.add("origami");
+        this.hobbies.add("surf");
+        this.hobbies.add("haikus");
     }
 
-    public Persona(String nombre, int edad) {
+    public Persona(String nombre, int edad, ArrayList<String> hobbies) {
         this.nombre = nombre;
         this.edad = edad;
+        this.hobbies = hobbies;
     }
 
     public String getNombre() {
@@ -29,10 +47,17 @@ public class Persona {
         this.edad = edad;
     }
 
-
+    /**
+     * Para poder comprobar los datos
+     * @return informacion de la persona
+     */
     @Override
     public String toString() {
-        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + "}";
+        StringBuilder info;
+        info = new StringBuilder(nombre + " de " + edad + ", tiene por hobbies:");
+        for (String hobby: hobbies)
+            info.append(" ").append(hobby);
+        return info.toString();
     }
 
 }
